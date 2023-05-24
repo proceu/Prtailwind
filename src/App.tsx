aed65fa059bef3bcb72eb226cca40a821cb69ba3
proceu <118274637+proceu@users.noreply.github.com>
2023-05-24 17:42:03 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import TodoList from "./TodoList"
+
+function App() {
+  return (
+    <div className="flex justify-center mt-20">
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
